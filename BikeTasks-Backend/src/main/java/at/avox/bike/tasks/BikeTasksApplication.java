38b688f58e93358c2cec30a2463c796e03cf09e7
Simon B. <47189894+simon-b64@users.noreply.github.com>
2026-05-03 11:12:39 +0200

    feat: Added baseline backend

---
+++ BikeTasks-Backend/src/main/java/at/avox/bike/tasks/BikeTasksApplication.java
@@ -0,0 +1,13 @@
+package at.avox.bike.tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeTasksApplication.class, args);
+	}
+
+}
---
+++ BikeTasks-Backend/src/test/java/at/avox/bike/tasks/BikeTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package at.avox.bike.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
