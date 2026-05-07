3117f972b825f53faab1a052b8eaace0c3ea0c56
Simon B. <nox@avox.at>
2026-05-07 16:39:19 +0200

    feat: Added linter and home page

--- BikeTasks-Frontend/src/app/app.routes.ts
+++ BikeTasks-Frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        component: HomePage,
+    },
+    {
+        path: '**',
+        redirectTo: '',
+    },
+];
